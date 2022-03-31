a1a42fb4a50f2a608ca870e31d597810f06e4613
zubritckiy Ivan <doshick203@yandex.ru>
2022-03-31 18:47:31 +0300

    journalist - ended

---
+++ js/journalist.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".search", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
