7b63c7f7073e55b231e5aa8bae79f659e9529b55
zubritckiy Ivan <doshick203@yandex.ru>
2022-04-02 23:12:57 +0300

    доделки переделки хедера

--- js/burger.js
+++ js/burger.js
@@ -22,3 +22,26 @@ function changeBodyScrolling(){
   let body = document.querySelector("body")
   body.classList.toggle("none-scroll")
 }
+
+
+
+let langBlockActive = document.querySelector(".header__lang-select__block.active");
+langBlockActive.onclick =function(){
+  openLangList();
+  langChange();
+}
+function openLangList(){
+  let langBlock = document.querySelector(".header__lang-select");
+  langBlock.classList.toggle('list-active');
+}
+function langChange(){
+  let langBloks = document.querySelectorAll(".header__lang-select__block");
+  for(let index = 0; index < langBloks.length; index++){
+    langBloks[index].onclick = function(){
+      langBlockActive.classList.remove("active");
+      langBlockActive = langBloks[index];
+      langBloks[index].classList.add("active");
+      openLangList()
+    }
+  }
+}
