67f265d735b7f80056bf022e3a85dfb1d02c3aed
zubritckiy Ivan <doshick203@yandex.ru>
2022-04-02 21:52:16 +0300

    service fix

--- js/services.js
+++ js/services.js
@@ -1,11 +1,22 @@
 var swiper = new Swiper(".search", {
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   type: "fraction",
-  // },
   slidesPerView: 1,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
+
+let serviceBtns = document.querySelectorAll(".breadcrumbs__block > button.breadcrumbs__btn")
+
+function ChangeActive(){
+  for(let index = 0; index < serviceBtns.length; index++){
+    serviceBtns[index].onclick = function(){
+      console.log(serviceBtns[index])
+      let activeBtn = document.querySelector(".breadcrumbs__block > .breadcrumbs__btn.active");
+      activeBtn.classList.remove("active")
+      serviceBtns[index].classList.add("active")
+    }
+  }
+}
+
+ChangeActive()
