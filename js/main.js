b04607e78a5bef3635c2b408d9c623a8d2d8a99a
zubritckiy Ivan <doshick203@yandex.ru>
2022-04-02 20:19:22 +0300

    our-services btns fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-// alert(window.innerWidth)
+let serviceBtns = document.querySelectorAll(".our-services__btns > button")
+// console.log(btnBlock)
 
-// function hola(a){
-//   let toto = 70 * a / 195;
-//   console.log(toto)
-// }
-// hola(150)
+function ChangeActive(){
+  for(let index = 0; index < serviceBtns.length; index++){
+    serviceBtns[index].onclick = function(){
+      let activeBtn = document.querySelector(".our-services__btns > button.active");
+      activeBtn.classList.remove("active")
+      serviceBtns[index].classList.add("active")
+    }
+  }
+}
+ChangeActive()
+
+
